0601992536799f18fe326325d7b1e2b9717b82f8
Nick Karaolis <nick.karaolis@digital.hmrc.gov.uk>
2017-07-20 15:51:43 +0100

    Fixed currying parameter issue by moving brackets onto same line Updated build dependencies Added required google analytics conf

--- app/controllers/HelloWorldController.scala
+++ app/controllers/HelloWorldController.scala
@@ -7,7 +7,7 @@ import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, AnyContent, Controller}
 
 @Singleton
-class HelloWorldController @Inject()(implicit configuration: AppConfig, val messagesApi: MessagesApi) extends Controller with I18nSupport {
+class HelloWorldController @Inject()(implicit configuration: FrontendConfig, val messagesApi: MessagesApi) extends Controller with I18nSupport {
 
 	def helloWorld(): Action[AnyContent] = Action {
 		implicit request => Ok(views.html.pages.hello_world())
