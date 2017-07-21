793d53a7a6016f2bba26ccad2fc0727666ba402f
Nick Karaolis <nick.karaolis@digital.hmrc.gov.uk>
2017-07-21 13:32:21 +0100

    Added common.conf configuration from frontend bootstrap Added global error messages Loaded the error template with the correct message depending on error type Added template route to get assets correctly Optimised imports and formatting in all files

--- app/config/FrontendConfig.scala
+++ app/config/FrontendConfig.scala
@@ -4,23 +4,21 @@ import javax.inject.Inject
 
 import play.api.Configuration
 
-
 trait AppConfig {
   val analyticsToken: String
   val analyticsHost: String
   val reportAProblemPartialUrl: String
   val reportAProblemNonJSUrl: String
-
 }
 
 class FrontendConfig @Inject()(config: Configuration) extends AppConfig {
-  private def loadConfig(key: String) = config.getString(key).getOrElse(throw new Exception(s"Missing configuration key: $key"))
+  private def loadConfString(key: String) = config.getString(key).getOrElse(throw new Exception(s"Missing configuration key: $key"))
 
-  private val contactHost = config.getString(s"contact-frontend.host").getOrElse("")
-  private val contactFormServiceIdentifier = loadConfig("appName")
+  private val contactHost = loadConfString(s"contact-frontend.host")
+  private val contactFormServiceIdentifier = loadConfString("appName")
 
-  lazy val analyticsToken = loadConfig("google-analytics.token")
-  lazy val analyticsHost = loadConfig("google-analytics.host")
+  lazy val analyticsToken: String = loadConfString("google-analytics.token")
+  lazy val analyticsHost: String = loadConfString("google-analytics.host")
   lazy val reportAProblemPartialUrl = s"$contactHost/contact/problem_reports_ajax?service=$contactFormServiceIdentifier"
   lazy val reportAProblemNonJSUrl = s"$contactHost/contact/problem_reports_nonjs?service=$contactFormServiceIdentifier"
 }
--- app/controllers/HelloWorldController.scala
+++ app/controllers/HelloWorldController.scala
@@ -2,15 +2,15 @@ package controllers
 
 import javax.inject._
 
-import config.{AppConfig, FrontendConfig}
+import config.FrontendConfig
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, AnyContent, Controller}
 
 @Singleton
 class HelloWorldController @Inject()(implicit configuration: FrontendConfig, val messagesApi: MessagesApi) extends Controller with I18nSupport {
 
-	def helloWorld(): Action[AnyContent] = Action {
-		implicit request => Ok(views.html.pages.hello_world())
-	}
+  def helloWorld(): Action[AnyContent] = Action {
+    implicit request => Ok(views.html.pages.hello_world())
+  }
 
 }
